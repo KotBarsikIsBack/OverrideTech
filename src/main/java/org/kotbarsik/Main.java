54bc0c95cd5acccc52cc7aa531199f0b1ac56469
KotBarsik <sadcatisback@gmail.com>
2023-11-08 21:25:50 +0300

    ass

--- src/main/java/org/kotbarsik/Main.java
+++ src/main/java/org/kotbarsik/Main.java
@@ -21,16 +21,21 @@ public class Main {
          }
 
          int startMoney = (startExpenses * 25 * 12) + (startExpenses * 3 * 12); // начальный капитал на вклад + текущий год
+
          System.out.println("Начальная сумма вкладов: " + startMoney);
+
          System.out.println("Расходы на месяц: " + startExpenses);
-         startMoney -= startExpenses;
+         startMoney -= startExpenses; // расходы на текущий год
          System.out.println("Начальная сумма вкладов c вычетом денег на жизнь в текущий год: " + startMoney);
+
          double startMoex = startMoney / Constants.MOEX_RATE[startYear];
          System.out.println("Начальное кол-во акций мосбиржи: " + startMoex);
-         double moex = startMoex * 1.15;
+
+         double moex = startMoex * 1.15; // доходы за год
          System.out.println("Доходы на текущий год: " + moex);
+
          int expenses = (int)(startExpenses * Constants.INFLATION_RATE[startYear]);
-         System.out.println("Траты на следующий год: " + expenses);
+         System.out.println("Траты на следующий год: " + expenses + ". В месяц: " + expenses/12);
 
 
 //           int startMoney = Constants.EXPENSES * 25 * 12 ; // 4%  12 month
@@ -42,7 +47,7 @@ public class Main {
 
          for (int i = startYear + 1; i < Constants.MOEX_RATE.length; i++ ){
              System.out.println(++date);
-             calculatePercent(0, moex, 0, i);
+             calculatePercent( moex, expenses, i);
             }
 
 
@@ -66,13 +71,20 @@ public class Main {
         return date;
     }
 
-    public static double calculatePercent(int startYear, double moex, int expenses, int index) {
-        double percent = 0;
+    public static double calculatePercent(double moex, int expenses, int index) {
+        //double percent = 0;
 
         int money = (int)(moex * Constants.MOEX_RATE[index]);
-        System.out.println(money);
+        System.out.println("Прирост акций 15%:" + money);
+        double percent = money * 0.04;
+        System.out.println("Доступные траты: " + percent + ". В месяц: " + percent / 12);
+        money -= expenses;
+        System.out.println("Остаток денег на вклад: " + money);
+        double newExpenses = expenses * Constants.INFLATION_RATE[index];
+        System.out.println("Траты не следующий год: " + newExpenses + ". В месяц: " + newExpenses/12);
+        return newExpenses;
 
-        return Math.round(percent * 2) / 2.0;
+       // return Math.round(percent * 2) / 2.0;
     }
 
 }
